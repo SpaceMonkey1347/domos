1ca9f4689791b335afc4a91024662ec47529c057
squid <SpaceMonkey1347@gmail.com>
2025-05-30 01:16:08 -0400

    fix perms in install and better uninstall

--- app/uninstall.bash
+++ app/uninstall.bash
@@ -4,16 +4,23 @@ SCRIPT_PATH="$(realpath "$0")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 VENV_DIR="$SCRIPT_DIR/venv"
 
-CHROME_GLOBAL_PATH="/etc/opt/chrome/native-messaging-hosts/com.example.uinput.json"
-CHROME_LOCAL_PATH="$HOME/.config/google-chrome/NativeMessagingHosts/com.example.uinput.json"
+CHROME_GLOBAL_DIR="/etc/opt/chrome/native-messaging-hosts"
+CHROME_LOCAL_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
 
-FIREFOX_GLOBAL_PATH="/usr/lib/mozilla/native-messaging-hosts/uinput.json"
-FIREFOX_LOCAL_PATH="$HOME/mozilla/native-messaging-hosts/uinput.json"
+FIREFOX_GLOBAL_DIR="/usr/lib/mozilla/native-messaging-hosts"
+FIREFOX_LOCAL_DIR="$HOME/mozilla/native-messaging-hosts"
+
+CHROME_GLOBAL_PATH="$CHROME_GLOBAL_DIR/com.example.uinput.json"
+CHROME_LOCAL_PATH="$CHROME_LOCAL_DIR/com.example.uinput.json"
+
+FIREFOX_GLOBAL_PATH="$FIREFOX_GLOBAL_DIR/uinput.json"
+FIREFOX_LOCAL_PATH="$FIREFOX_LOCAL_DIR/uinput.json"
 
 echo "Checking for: $CHROME_GLOBAL_PATH"
 if [[ -f "$CHROME_GLOBAL_PATH" ]]; then
     echo "Deleting file..."
     sudo rm $CHROME_GLOBAL_PATH
+    sudo rmdir -p $CHROME_GLOBAL_DIR --ignore-fail-on-non-empty
 else
     echo "File does not exist"
 fi
@@ -22,6 +29,7 @@ echo "Checking for: $CHROME_LOCAL_PATH"
 if [[ -f "$CHROME_LOCAL_PATH" ]]; then
     echo "Deleting file..."
     rm $CHROME_LOCAL_PATH
+    rmdir -p $CHROME_LOCAL_DIR --ignore-fail-on-non-empty
 else
     echo "File does not exist"
 fi
@@ -30,6 +38,7 @@ echo "Checking for: $FIREFOX_GLOBAL_PATH"
 if [[ -f "$FIREFOX_GLOBAL_PATH" ]]; then
     echo "Deleting file..."
     sudo rm $FIREFOX_GLOBAL_PATH
+    sudo rmdir -p $FIREFOX_GLOBAL_DIR --ignore-fail-on-non-empty
 else
     echo "File does not exist"
 fi
@@ -38,6 +47,9 @@ echo "Checking for: $FIREFOX_LOCAL_PATH"
 if [[ -f "$FIREFOX_LOCAL_PATH" ]]; then
     echo "Deleting file..."
     rm $FIREFOX_LOCAL_PATH
+    rmdir -p $FIREFOX_LOCAL_DIR --ignore-fail-on-non-empty
 else
     echo "File does not exist"
 fi
+
+rm -rf $SCRIPT_DIR/venv
